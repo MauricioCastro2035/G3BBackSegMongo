69483d718c4c9ca9fa08a5dbaf63ff89fa137e2a
Mauricio_Castro_Castro81 <mauricio.castro.castro2@gmail.com>
2022-09-18 21:31:06 -0500

    Con relación uno a muchos entre usuario y rol ya funcionando Con relacion muchos a muchos entre rol y permiso

--- src/main/java/Tutorial/misionTIC/seguridad/SeguridadApplication.java
+++ src/main/java/Tutorial/misionTIC/seguridad/SeguridadApplication.java
@@ -3,6 +3,7 @@ package Tutorial.misionTIC.seguridad;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SeguridadApplication {
 
