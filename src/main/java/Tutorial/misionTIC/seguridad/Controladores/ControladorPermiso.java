05c0174f02fd9e247d2e25485d183d18fb584445
Mauricio_Castro_Castro81 <mauricio.castro.castro2@gmail.com>
2022-09-19 17:54:02 -0500

    Modificado consultar permiso individual por ID

--- src/main/java/Tutorial/misionTIC/seguridad/Controladores/ControladorPermiso.java
+++ src/main/java/Tutorial/misionTIC/seguridad/Controladores/ControladorPermiso.java
@@ -1,5 +1,6 @@
 package Tutorial.misionTIC.seguridad.Controladores;
 import Tutorial.misionTIC.seguridad.Modelos.Permiso;
+import Tutorial.misionTIC.seguridad.Modelos.Rol;
 import Tutorial.misionTIC.seguridad.Repositorios.RepositorioPermiso;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,15 @@ public class ControladorPermiso {
     public List<Permiso> index(){
         return this.miRepositorioPermiso.findAll();
     }
+
+    @GetMapping("{id}")
+    public Permiso index2(@PathVariable String id){
+        Permiso permisoActual=this.miRepositorioPermiso
+                .findById(id)
+                .orElse(null);
+        return permisoActual;
+    }
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public Permiso create(@RequestBody Permiso infoPermiso){
